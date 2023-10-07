06b042f590d34c1c62ddb0a84ce198e06f3329cc
César Manuel Varela García <manu220890@gmail.com>
2023-10-07 20:24:12 +0200

    test 2

--- eklipse.py
+++ eklipse.py
@@ -1,14 +1,37 @@
-import shutil
+import folium
 
-# Lee la plantilla HTML
-with open('template.html', 'r') as file:
-    template_content = file.read()
+# Coordenadas iniciales (Urduliz, Vizcaya, España)
+urduliz_coords = [43.3970, -2.9770]
 
-# Guarda el contenido modificado en un nuevo archivo
-with open('mapa_urduliz.html', 'w') as file:
-    file.write(template_content)
+# Coordenadas de un polígono que representa una zona señalada
+zona_coords = [
+    [43.3980, -2.9790],
+    [43.3980, -2.9750],
+    [43.3950, -2.9750],
+    [43.3950, -2.9790]
+]
 
-# Mueve el archivo a la carpeta adecuada para servirlo
-shutil.move('mapa_urduliz.html', 'docs/mapa_urduliz.html')
+# Crear un mapa centrado en Urduliz
+m = folium.Map(location=urduliz_coords, zoom_start=14)
+
+# Añadir un marcador para Urduliz
+folium.Marker(
+    location=urduliz_coords,
+    popup='Urduliz, Vizcaya, España',
+    icon=folium.Icon(icon='cloud')
+).add_to(m)
+
+# Añadir un polígono para la zona señalada
+folium.Polygon(
+    locations=zona_coords,
+    color='red',
+    fill=True,
+    fill_color='red',
+    fill_opacity=0.4,
+    popup='Zona señalada'
+).add_to(m)
+
+# Guardar el mapa en la carpeta 'docs' como 'mapa_urduliz.html'
+m.save('docs/mapa_urduliz.html')
 
 print("Mapa generado y guardado como 'docs/mapa_urduliz.html'")
